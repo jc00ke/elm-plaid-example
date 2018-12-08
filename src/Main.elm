618869cb8acd64dc21c2d64203519394825af413
Jesse Cooke <jesse@jc00ke.com>
2018-12-07 16:09:16 -0800

    Wire up button to open Plaid Link

--- src/Main.elm
+++ src/Main.elm
@@ -1,128 +1,46 @@
+port module Main exposing (..)
+
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
-import Json.Decode exposing (
-  Decoder,
-  at,
-  field,
-  int,
-  lazy,
-  list,
-  map,
-  map2,
-  map3,
-  map4,
-  map5,
-  string)
-
-
 
 -- MAIN
 
 
 main =
   Browser.element {
-    init = init,
-    update = update,
+    init = \() -> ( initialModel, Cmd.none ),
     subscriptions = subscriptions,
+    update = update,
     view = view
+  }
+
+initialModel : Model
+initialModel =
+  { items = []
+  , consentToNotify = True
+  , consentForTransactions = False
   }
 
 
 
 -- MODEL
 
-type alias Account =
-  { id : String
-  , name : String
-  , mask : String
-  , typeName : String
-  , subtype : String
-  }
-
-type alias Institution =
-  { name : String
-  , id : String
-  }
-
-type alias Metadata =
-  { linkSessionId : String
-  , publicToken : String
-  , institution : Institution
-  , primaryAccount : Account
-  , accounts : Accounts
-  }
-
-type Accounts = Accounts (List Account)
 
 type alias Model =
-  { items : List Metadata
+  { items : List String
   , consentToNotify : Bool
   , consentForTransactions : Bool
   }
 
 
 
--- DECODER
-
-account : Decoder Account
-account =
-  map5 Account
-    (field "id" string)
-    (field "name" string)
-    (field "mask" string)
-    (field "typeName" string)
-    (field "subtype" string)
-
-institution : Decoder Institution
-institution =
-  map2 Institution
-  (field "id" string)
-  (field "name" string)
-
-metadata : Decoder Metadata
-metadata =
-  map5 Metadata
-  (field "link_session_id" string)
-  (field "public_token" string)
-  (field "institution" institution)
-  (field "account" account)
-  (field "accounts" (map Accounts (list (lazy (\_ -> account)))))
-
-
-init : () -> (Model, Cmd Msg)
+init : () -> (Model, Cmd msg)
 init _ =
-  ( Model [] True True
-  , Cmd.none )
+  ( initialModel, Cmd.none )
 
-
-
--- UPDATE
-
-
-type Msg
-  = ToggleConsentToNotify
-  | ToggleConsentForTransactions
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-  case msg of
-    ToggleConsentToNotify ->
-      (
-        { model | consentToNotify = not model.consentToNotify }
-        , Cmd.none
-      )
-
-    ToggleConsentForTransactions ->
-      (
-        { model | consentForTransactions = not model.consentForTransactions }
-        , Cmd.none
-      )
-
-
-
--- SUBSCRIPTIONS
+port openPlaidLink : String -> Cmd msg
 
 
 subscriptions : Model -> Sub Msg
@@ -130,20 +48,58 @@ subscriptions model =
   Sub.none
 
 
+
+type Msg
+  = OpenPlaidLink
+  | ToggleConsentToNotify
+  | ToggleConsentForTransactions
+  | Loaded
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    Loaded ->
+      ( model, openPlaidLink "Loaded!" )
+
+    OpenPlaidLink ->
+      ( model, openPlaidLink "Open!" )
+
+    ToggleConsentToNotify ->
+      (
+        { model | consentToNotify = not model.consentToNotify }
+      , Cmd.none
+      )
+
+
+    ToggleConsentForTransactions ->
+      (
+        { model | consentForTransactions = not model.consentForTransactions }
+      , Cmd.none
+      )
+
+
+
 -- VIEW
 
 
 view : Model -> Html Msg
 view model =
-  div [] []
-  --fieldset []
-    --[ checkbox ToggleConsentToNotify "Consent to receive SMS messages"
-    --, checkbox ToggleConsentForTransactions "Consent to give us your transaction history"
-    --]
+  div []
+      [ div [] [ checkbox ToggleConsentToNotify "Receive SMS from us" ]
+      , div [] [ checkbox ToggleConsentForTransactions "Make your transaction history available" ]
+      , div [] [ button [ onClick OpenPlaidLink ] [ text "Link Account" ] ]
+      ]
 
 checkbox : msg -> String -> Html msg
 checkbox msg name =
   label []
-    [ input [ type_ "checkbox", onClick msg ] []
-    , text name
-    ]
+        [
+          input [type_ "checkbox", onClick msg] []
+        , text name
+        ]
+
